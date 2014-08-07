7327eb40ee8954416883a1601746a8d8adc92b5d
slamflipstrom <lindstrom.s@gmail.com>
2014-08-06 23:08:38 -0500

    2nd Commit
    Many fixes - Program now processes searches and outputs movie title
    with release date.

--- lib/main.rb
+++ lib/main.rb
@@ -15,27 +15,25 @@ get "/" do
 end
 
 # Performs the search and provides backend with user's input
-post "/" do
-@movie = Tmdb::Movie.find(params[:query])
+get "/results" do
+@search = Tmdb::Movie.find(params[:query])
+@query = params[:query]
+
+erb :selection
   
-  movie.id = params["movie.id"]
-  movie.title = params["movie.title"]
-  movie.tagline = params["movie.tagline"]
-  movie.imdb_id = params["movie.imdb_id"]
-  movie.genres = params ["movie.genres"]
-  movie.popularity = params["movie.popularity"]
-  movie.release_date = params["movie.realease_date"]
-  movie.runtime = params["movie.runtime"]
+  # movie.id = params["movie.id"]
+ #  movie.title = params["movie.title"]
+ #  movie.tagline = params["movie.tagline"]
+ #  movie.imdb_id = params["movie.imdb_id"]
+ #  movie.genres = params ["movie.genres"]
+ #  movie.popularity = params["movie.popularity"]
+ #  movie.release_date = params["movie.realease_date"]
+ #  movie.runtime = params["movie.runtime"]
 end
   
 
 # Takes user to the Movie they are searching for.
-get "/:search" do
-  
-  
 
-erb :selection
-end
 
 # binding.pry
 
